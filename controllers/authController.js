13c01a2ab4a3adbda8869f879fecfb70fad8643c
kb603 <kojoaddobaffour@gmail.com>
2025-07-06 18:03:12 +0000

    updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,53 +1,48 @@
-const User = require("../models/User");
+import User from "../models/User.js";
 
-// handle errors
-const handleErrors = (err) => {
-  console.log(err.message, err.code);
+const handleErrors = (error) => {
+  console.log(error.message, error.code);
   let errors = { email: "", password: "" };
 
   // duplicate email error
-  if (err.code === 11000) {
+  if (error.code === 11000) {
     errors.email = "that email is already registered";
     return errors;
   }
 
   // validation errors
   if (err.message.includes("user validation failed")) {
-    // console.log(err);
     Object.values(err.errors).forEach(({ properties }) => {
-      // console.log(val);
-      // console.log(properties);
       errors[properties.path] = properties.message;
     });
   }
-
   return errors;
 };
 
-// controller actions
-module.exports.signup_get = (req, res) => {
+export function getSignupRoute(req, res) {
   res.render("signup");
-};
+}
 
-module.exports.login_get = (req, res) => {
-  res.render("login");
-};
-
-module.exports.signup_post = async (req, res) => {
+export async function postSignupRoute(req, res) {
   const { email, password } = req.body;
 
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user);
-  } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
+  } catch (error) {
+    console.log(error);
+    handleErrors(error);
+    res.status(400).send("User not created");
   }
-};
+}
 
-module.exports.login_post = async (req, res) => {
+export function getLoginRoute(req, res) {
+  res.render("login");
+}
+
+export function postLoginRoute(req, res) {
   const { email, password } = req.body;
-
   console.log(email, password);
+
   res.send("user login");
-};
+}
